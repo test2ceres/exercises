c87b000205adca43e883d3ab4e786ac54dedf197
HAN Dang <han.dang@ceresolutions.com>
2018-01-08 17:38:47 +0700

    update exe9

--- basic/ExercisesNine.php
+++ basic/ExercisesNine.php
@@ -9,5 +9,6 @@ echo $string . "<br>";
 
 //TODO: use same preg_replace function to change color of first word on string.
 $string2 = "find first of word on string";
-$string2 = preg_replace('/find/', '<span style="color:blue;">\0</span>', $string2);
+$arr = explode(' ', $string2);
+$string2 = preg_replace('/('.$arr[0].')/', '<span style="color:blue;">\1</span>', $string2);
 echo $string2;
